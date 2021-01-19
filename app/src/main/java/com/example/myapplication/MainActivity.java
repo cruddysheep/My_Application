dc5f4f8f687a4eb424a6c39b324f326459b455c8
cruddysheep <ratkovecalex@gmail.com>
2021-01-18 23:31:30 -0800

    second

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+/*
+    Alex Ratkovec
+    Pre-work for codepath.org
+    January 18,2021
+*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // User can tap a button to change the text color of the label.
+        findViewById(R.id.colorButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // change the text color of the label
+                ((TextView) findViewById(R.id.nameColorChange)).setTextColor(getResources().getColor(R.color.white));
+            }
+        });
+
+
     }
 }
